05938546da00233d99299c2663f15f0380d18ce9
Parengninyo <endosoninochristopher@gmail.com>
2024-12-07 09:58:30 +0800

    endoso super jump

--- bitlandia.py
+++ bitlandia.py
@@ -2,6 +2,7 @@
 import pygame
 import random
 import os
+from pygame import mixer
 from spritesheet import SpriteSheet
 from enemy import Enemy
 
@@ -39,6 +40,11 @@ game_over = False
 score = 0
 fade_counter = 0
 
+#score multiplier and super jump variables
+score_multiplier = 1
+multiplier_timer = 0
+super_jump_active = False
+
 if os.path.exists('score.txt'):
     with open('score.txt', 'r') as file:
         high_score = int(file.read())
@@ -58,6 +64,8 @@ font_big = pygame.font.SysFont('Lucida Sans', 24)
 bitlandia_image = pygame.image.load('assets/images/character/run/character_berie_run_1.png').convert_alpha()
 bg_image = pygame.image.load('assets/images/background/Bg.png').convert_alpha()
 platform_image = pygame.image.load('assets/images/platform/tilemap2.png').convert_alpha()
+item_image = pygame.image.load('assets/images/item/score.png').convert_alpha()
+super_jump_image = pygame.image.load('assets/images/item/super jump.png').convert_alpha()
 #bird spritesheet
 bird_sheet_img = pygame.image.load('assets/images/enemy/bird.png').convert_alpha()
 bird_sheet = SpriteSheet(bird_sheet_img)
@@ -72,6 +80,12 @@ def draw_panel():
     pygame.draw.rect(screen, PANEL, (0, 0, SCREEN_WIDTH, 30))
     pygame.draw.line(screen, WHITE, (0, 30), (SCREEN_WIDTH, 30), 2)
     draw_text('SCORE: ' + str(score), font_small, WHITE, 0, 0)
+
+    # Display multiplier and super jump status
+    if score_multiplier > 1:
+        draw_text('2x SCORE', font_small, WHITE, SCREEN_WIDTH - 150, 0)
+    if super_jump_active:
+        draw_text('SUPER JUMP READY!', font_small, WHITE, SCREEN_WIDTH - 150, 20)
 
 #function for drawing the background
 def draw_bg(bg_scroll):
@@ -90,6 +104,8 @@ class Player():
         self.flip = False
 
     def move(self):
+        global super_jump_active  # Declare as global to modify it
+
         #reset variables
         scroll = 0
         dx = 0
@@ -123,7 +139,12 @@ class Player():
                     if self.vel_y > 0:
                         self.rect.bottom = platform.rect.top
                         dy = 0
-                        self.vel_y = -20
+                        # Trigger super jump effect if active
+                        if super_jump_active:
+                            self.vel_y = -40  # Super jump height
+                            super_jump_active = False  # Reset after one use
+                        else:
+                            self.vel_y = -20  # Normal jump height
                         jump_fx.play()
 
         #check if the player has bounced to the top of the screen
@@ -146,32 +167,53 @@ class Player():
 
 #platform class
 class Platform(pygame.sprite.Sprite):
-	def __init__(self, x, y, width, moving):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.transform.scale(platform_image, (width, 10))
-		self.moving = moving
-		self.move_counter = random.randint(0, 50)
-		self.direction = random.choice([-1, 1])
-		self.speed = random.randint(1, 2)
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
+    def __init__(self, x, y, width, moving):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(platform_image, (width, 20))
+        self.moving = moving
+        self.move_counter = random.randint(0, 50)
+        self.direction = random.choice([-1, 1])
+        self.speed = random.randint(1, 2)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-	def update(self, scroll):
-		#moving platform side to side if it is a moving platform
-		if self.moving == True:
-				self.move_counter +=1
-				self.rect.x += self.direction * self.speed
+    def update(self, scroll):
+        # If platform is moving, update its position
+        if self.moving:
+            self.rect.x += self.direction * self.speed
+            if self.rect.left <= 0 or self.rect.right >= SCREEN_WIDTH:
+                self.direction *= -1  # Reverse direction at edges
 
-		#change the platform direction if it has moved fully hit a wall
-		if self.move_counter >= 100 or self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
-				self.direction *= -1
-				self.move_counter - 0
-  
-		#update platform's vertical position
-		self.rect.y += scroll
+        # Update platform's vertical position
+        self.rect.y += scroll
 
         # Remove platform if it goes off-screen
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
+
+#item classes
+class Item(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(item_image, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self, scroll):
+        self.rect.y += scroll
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
+
+class SuperJumpItem(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(super_jump_image, (60, 60))  # Larger size
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self, scroll):
+        self.rect.y += scroll
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
 
@@ -181,6 +223,8 @@ bitlandia = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 #create sprite groups
 platform_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
+item_group = pygame.sprite.Group()
+super_jump_group = pygame.sprite.Group()
 
 #create starting platform
 platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100, False)
@@ -201,80 +245,126 @@ while run:
             bg_scroll = 0
         draw_bg(bg_scroll)
 
-		#generate platforms
-		if len(platform_group) < MAX_PLATFORMS:
-			p_w = random.randint(40, 60)
-			p_x = random.randint(0, SCREEN_WIDTH - p_w)
-			p_y = platform.rect.y - random.randint(80, 120)
-			p_type = random.randint(1, 2)
-			if p_type == 1 and score > 500:
-					p_moving = True
-			else:
-					p_moving = False
-			platform = Platform(p_x, p_y, p_w, p_moving)
-			platform_group.add(platform)
+        #generate platforms
+        if len(platform_group) < MAX_PLATFORMS:
+            p_w = random.randint(40, 60)
+            p_x = random.randint(0, SCREEN_WIDTH - p_w)
+            p_y = platform.rect.y - random.randint(80, 120)
+            p_type = random.randint(1, 2)
+            if p_type == 1 and score > 500:
+                p_moving = True
+            else:
+                p_moving = False
+            platform = Platform(p_x, p_y, p_w, p_moving)
+            platform_group.add(platform)
 
-		#update platforms
-		platform_group.update(scroll)
+            # Spawn score item
+            if score > 200 and not p_moving and random.randint(1, 5) == 1:
+                item = Item(p_x + p_w // 2, p_y - 10)
+                item_group.add(item)
+
+            # Spawn super jump item
+            if score > 200 and not p_moving and random.randint(1, 5) == 1:
+                super_jump_item = SuperJumpItem(p_x + p_w // 2, p_y - 10)
+                super_jump_group.add(super_jump_item)
+
+        #update platforms
+        platform_group.update(scroll)
+
+        #update items
+        item_group.update(scroll)
+        super_jump_group.update(scroll)
+
+        #check for item collection
+        for item in item_group:
+            if bitlandia.rect.colliderect(item.rect):
+                score_multiplier = 2
+                multiplier_timer = 300
+                item.kill()
+
+        #check for super jump collection
+        for item in super_jump_group:
+            if bitlandia.rect.colliderect(item.rect):
+                super_jump_active = True  # Activate super jump for one jump
+                item.kill()
+
+        #update multiplier timer
+        if multiplier_timer > 0:
+            multiplier_timer -= 1
+        else:
+            score_multiplier = 1
 
         #generate enemies
         if len(enemy_group) == 0 and score > 1500:
             enemy = Enemy(SCREEN_WIDTH, 100, bird_sheet, 1.5)
             enemy_group.add(enemy)
-    
-		#update enemy
-		enemy_group.update(scroll, SCREEN_WIDTH)
-  
-		#update score
-		if scroll > 0:
-			score += scroll
+
+        #update enemies
+        enemy_group.update(scroll, SCREEN_WIDTH)
+
+        #update score
+        if scroll > 0:
+            score += scroll * score_multiplier
 
         #draw line at previous high score
         pygame.draw.line(screen, WHITE, (0, score - high_score + SCROLL_THRESH), (SCREEN_WIDTH, score - high_score + SCROLL_THRESH), 3)
         draw_text('HIGH SCORE', font_small, WHITE, SCREEN_WIDTH - 130, score - high_score + SCROLL_THRESH)
 
-		#draw sprites
-		platform_group.draw(screen)
-		enemy_group.draw(screen)
-		bitlandia.draw()
+        #draw sprites
+        platform_group.draw(screen)
+        enemy_group.draw(screen)
+        item_group.draw(screen)
+        super_jump_group.draw(screen)
+        bitlandia.draw()
 
         #draw panel
         draw_panel()
 
-		#check game over
-		if bitlandia.rect.top > SCREEN_HEIGHT:
-			game_over = True
-	else:
-		if fade_counter < SCREEN_WIDTH:
-			fade_counter += 5
-			for y in range(0, 6, 2):
-				pygame.draw.rect(screen, BLACK, (0, y * 100, fade_counter, 100))
-				pygame.draw.rect(screen, BLACK, (SCREEN_WIDTH - fade_counter, (y + 1) * 100, SCREEN_WIDTH, 100))
-		else:
-			draw_text('GAME OVER!', font_big, WHITE, 130, 200)
-			draw_text('SCORE: ' + str(score), font_big, WHITE, 130, 250)
-			draw_text('PRESS SPACE TO PLAY AGAIN', font_big, WHITE, 40, 300)
-			#update high score
-			if score > high_score:
-				high_score = score
-				with open('score.txt', 'w') as file:
-					file.write(str(high_score))
-			key = pygame.key.get_pressed()
-			if key[pygame.K_SPACE]:
-				#reset variables
-				game_over = False
-				score = 0
-				scroll = 0
-				fade_counter = 0
-				#reposition bitlandia
-				bitlandia.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
-				#reset enemies
-				enemy_group.empty()
-				#reset platforms
-				platform_group.empty()
-				#create starting platform
-				platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100, False)
-				platform_group.add(platform)
+        #check game over
+        if bitlandia.rect.top > SCREEN_HEIGHT:
+            game_over = True
+            death_fx.play()
+        #check for collision with enemies
+        if pygame.sprite.spritecollide(bitlandia, enemy_group, False):
+            if pygame.sprite.spritecollide(bitlandia, enemy_group, False, pygame.sprite.collide_mask):
+                game_over = True
+                death_fx.play()
+    else:
+        if fade_counter < SCREEN_WIDTH:
+            fade_counter += 5
+            for y in range(0, 6, 2):
+                pygame.draw.rect(screen, BLACK, (0, y * 100, fade_counter, 100))
+                pygame.draw.rect(screen, BLACK, (SCREEN_WIDTH - fade_counter, (y + 1) * 100, SCREEN_WIDTH, 100))
+        else:
+            draw_text('GAME OVER!', font_big, WHITE, 130, 200)
+            draw_text('SCORE: ' + str(score), font_big, WHITE, 130, 250)
+            draw_text('PRESS SPACE TO PLAY AGAIN', font_big, WHITE, 40, 300)
+            #update high score
+            if score > high_score:
+                high_score = score
+                with open('score.txt', 'w') as file:
+                    file.write(str(high_score))
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                #reset variables
+                game_over = False
+                score = 0
+                scroll = 0
+                fade_counter = 0
+                score_multiplier = 1
+                super_jump_active = False
+                #reposition bitlandia
+                bitlandia.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+                #reset enemies
+                enemy_group.empty()
+                #reset platforms
+                platform_group.empty()
+                #reset items
+                item_group.empty()
+                super_jump_group.empty()
+                #create starting platform
+                platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100, False)
+                platform_group.add(platform)
 
     #event handler
     for event in pygame.event.get():
